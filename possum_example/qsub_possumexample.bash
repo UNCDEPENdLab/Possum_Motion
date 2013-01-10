d5aebfb668b8af3192c95b2e7a4bf65bd4083828
Michael Hallquist <hallquis@tg-login1.blacklight.psc.teragrid.org>
2013-01-10 14:11:22 -0500

    Finalize example qsub

--- possum_example/qsub_possumexample.bash
+++ possum_example/qsub_possumexample.bash
@@ -32,39 +32,42 @@ echo "Host:    $HOSTNAME"
 ### Possum for each job id ###
 ##############################
 
+FSLOUTPUTTYPE=NIFTI_GZ
+PATH=$HOME/Possum_Motion/bin/linux:${PATH}
+export PATH FSLOUTPUTTYPE
+
 [ ! -f $inputDir/example_pulse ] && source $inputDir/generate_example_pulse.bash
 
+which ja && ja
+
 IFS=:
-for ((jobID=1; jobID <= $ncpus ; jobID++))
+for ((jobID=1; jobID <= ncpus ; jobID++)); do
 
    # job completion check/parse requires the log file be named only a number
    LogFile="$LogDir/${jobID}"
 
-   let jobID--  #possum is zero based, the log structure is not!
+   let "jobID_0 = jobID - 1"  #possum is zero based, the log structure is not!
 
    # run or print out what we would run
    if [ "$REALLYRUN" == "1" ]; then
 
-      which ja && ja
-
       set -x
       possum                               \
-          --nproc=$$ncpus                  \
-          --procid=$jobID                  \
+          --nproc=$ncpus                   \
+          --procid=$jobID_0                \
           -o $SimOutDir/possum_${jobID}    \
           -m $inputDir/zeromotion          \
           -i $inputDir/brain.nii.gz        \
           -x $inputDir/MRpar_3T            \
           -f $inputDir/slcprof             \
           -p $inputDir/example_pulse       \
-          --activ=$inputDir/activation.nii.gz      \
+          --activ=$inputDir/activation3D.nii.gz     \
           --activt=$inputDir/activation3Dtimecourse \
             > $LogFile &
       set +x
 
-
       #[[ $HOSTNAME =~ blacklight ]] && ja -chlst > $QueLogDir/${simID}_${jobID}.job.log
-      which ja &&  ja -chlst > $QueLogDir/${simID}_${jobID}.job.log
+      #which ja &&  ja -chlst > $QueLogDir/${simID}_${jobID}.job.log
 
       #-c command report
       #-h Kilobytes of largest memory usage
@@ -74,16 +77,16 @@ for ((jobID=1; jobID <= $ncpus ; jobID++))
    else
       ## testing: just say we got here
       echo 
-      echo possum                               \
-          --nproc=$$ncpus                  \
-          --procid=$jobID                  \
+      echo possum                          \
+          --nproc=$ncpus                   \
+          --procid=$jobID_0                \
           -o $SimOutDir/possum_${jobID}    \
           -m $inputDir/zeromotion          \
           -i $inputDir/brain.nii.gz        \
           -x $inputDir/MRpar_3T            \
           -f $inputDir/slcprof             \
           -p $inputDir/example_pulse       \
-          --activ=$inputDir/activation.nii.gz      \
+          --activ=$inputDir/activation3D.nii.gz     \
           --activt=$inputDir/activation3Dtimecourse \
             ">" $LogFile
       echo

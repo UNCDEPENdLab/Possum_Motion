d08772f23491228b95ecd67d76e9552f80469cc4
Will Foran <foranw@tg-login1.blacklight.psc.teragrid.org>
2013-01-25 12:33:11 -0500

    include example que

---
+++ queIncomplete/finish_2013-01-25/possumRun.bash
@@ -0,0 +1,92 @@
+#!/usr/bin/env sh
+
+# possum settings
+
+
+source /usr/share/modules/init/bash
+
+njobs=256
+
+inputDir=$HOME/Possum_Motion/defaults
+motionDir=$inputDir/motion_parameters
+
+function dircheck   { eval   dirt=\$$1;    [ -d "$dirt"   ] || mkdir -p $dirt; }
+
+
+
+echo "SCRATCH:    $SCRATCH"
+echo "Host:       $HOSTNAME"
+
+# assume these are all ready from sim_cfg .. which is loaded per possum process (rather than here, per job)
+#defaults, if not set in the sim cfg
+#[ -z "$motion" ]    &&    motion="$motionDir/zeromotion"
+#[ -z "$t1input" ]   &&   t1input="$inputDir/possum_10895_fast.nii.gz"
+#[ -z "$activ4D" ]   &&   activ4D="$inputDir/10895_POSSUM4D_bb264_roiAvg_fullFreq.nii.gz"
+#[ -z "$activTime" ] && activTime="$inputDir/activt_150"
+#[ -z "$mrPar" ]     &&     mrPar="$inputDir/MRpar_3T"
+#[ -z "$slcprof" ]   &&   slcprof="$inputDir/slcprof"
+#[ -z "$pulse" ]     &&     pulse="$inputDir/tr2_te30_pulse"
+#[ ! -f $inputDir/tr2_te30_pulse ] && bash $inputDir/default_pulse.bash
+
+##############################
+### Possum for each job id ###
+##############################
+
+FSLOUTPUTTYPE=NIFTI_GZ
+PATH=$HOME/Possum_Motion/bin/linux:${PATH}
+export PATH FSLOUTPUTTYPE
+
+
+which ja && ja
+
+
+
+## RUN FUNCTION 
+# possum log numbers start at one, proc ids start at 0
+function possumRun { 
+   SIMRUN=$2
+   cfgfile="$HOME/Possum_Motion/sim_cfg/$SIMRUN"
+   expectedRuntime=$3
+
+
+
+   if [ -n "$SIMRUN" -a -r "$cfgfile" ]; then
+       source $cfgfile
+       simname=${SIMRUN}_$(date +%d%b%Y-%R)
+   else
+       echo "$1: '$SIMRUN' '$cfgfile' DNE!!!! dieing"
+       return
+   fi
+
+   LogDir="$SCRATCH/possum_rsfcmri/$simname/logs"
+   SimOutDir="$SCRATCH/possum_rsfcmri/$simname/output"
+
+   #  check for log file, make if DNE
+   dircheck "LogDir"
+   dircheck "SimOutDir"
+
+   LogFile="$LogDir/possumlog_$1"
+
+   echo "OutputDir:  $SimOutDir"
+   echo "LogDir:     $LogDir"
+   echo "SIMRUN:     $SIMRUN"
+
+
+   jobID_0=$(echo $1 - 1|bc)
+   echo  "expected runtime: $expectedRuntime";
+   echo -n "start: "; date
+   echo possum                               \
+           --nproc=$njobs \
+           --procid=$jobID_0 \
+           -o $SimOutDir/possum_${jobID_0} \
+           -m $motion \
+           -i $t1input \
+           -x $mrPar \
+           -f $slcprof \
+           -p $pulse \
+           --activ4D=$activ4D \
+           --activt4D=$activTime \
+           \> $LogFile 
+   echo -n "finished: "; date
+}
+

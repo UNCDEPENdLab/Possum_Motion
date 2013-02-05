655f88735955fa2d18e565b393e7ce9289166ce0
Michael Hallquist <hallquis@tg-login1.blacklight.psc.teragrid.org>
2013-02-05 11:38:44 -0500

    Interim commits of combine_output improvements, added 153 vol pulse for older sims

---
+++ defaults/default_pulse_153vol.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+scriptDir=$(echo $(cd $(dirname $0); pwd) )
+pulse -i ${scriptDir}/possum_10895_fast.nii.gz -o ${scriptDir}/tr2_te30_pulse_153vol --te=0.03 --tr=2.00 \
+    --trslc=0.0665 --nx=60 --ny=60 --dx=0.0033 --dy=0.0033 \
+    --maxG=0.055 --riset=0.00022 --bw=221000 \
+    --numvol=153 --numslc=30 --slcthk=0.004 --zstart=0.040 \
+    --seq=epi --slcdir=z+ --readdir=x+ \
+    --phasedir=y+ --gap=0.0 -v --cover=100 --angle=90

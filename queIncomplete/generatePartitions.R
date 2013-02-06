51b78d7fab302aa75edb3503187de5d3f1f7373c
Will Foran <willforan@gmail.com>
2013-02-06 11:42:57 -0500

    fix: actually remove times that are over

--- queIncomplete/generatePartitions.R
+++ queIncomplete/generatePartitions.R
@@ -58,10 +58,12 @@ times.unfin$knownExample[noinfo] <- knownExMean
 
 remain      <- apply(cbind(times.unfin$expectedsec,times.unfin$knownExample)/60**2,1,max,na.rm=T)
 remain[remain==-Inf] <- knownExMean #mean(times$expectedsec[times$expectedsec>1*60^2],na.rm=T)/60**2
-times       <- times.unfin[remain<maxTimehr,]
-if(dim(times)[1]!=dim(times.unfin)[1]) {
+overidx     <- which(remain<maxTimehr)
+times       <- times.unfin[-overidx,]
+remain <- remain[-overidx]
+if(length(overidx)>0) {
  cat("DROPPED jobs are expted to take over 100 hours\n")
- print(times.unfin[remain>=110,c('sim_cfg','poss_logfile','expectedsec')] )
+ print(times.unfin[overidx,c('sim_cfg','poss_logfile','expectedsec')] )
 }
 # sort(remain,index.return=T)
 

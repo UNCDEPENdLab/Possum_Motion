0c93d9b26302536c1d6bfccb8eccf3d818195242
Michael Hallquist <hallquis@tg-login1.blacklight.psc.teragrid.org>
2013-02-21 21:17:50 -0500

    more machinery for the 'hold' approach for activation time series

--- defaults/double4d.bash
+++ defaults/double4d.bash
@@ -2,11 +2,21 @@
 
 #double the 150 activation for the "hold" approach to activation (where there is little interpolation from one TR to the next because the two volumes with identical activations are used to "hold" a value).
 
-fslsplit 10895_POSSUM4D_bb264_roiAvg_fullFreq_x5.nii.gz tcat
-mergcmd='fslmerge -tr 10895_POSSUM4D_bb264_roiAvg_fullFreq_x5_DOUBLE.nii.gz '
+what="$1"
+#echo "what: $what"
+
+[ -z "$what" ] && echo "Did not specify what image to double. Pass as first parameter." && exit 1
+[ ! -r "$what" ] && echo "File $what does not exist." && exit 1
+
+cd $( dirname "$what" )
+
+fslsplit "$what" tcat
+mergecmd="fslmerge -tr $( remove_ext \"$what\" )_DOUBLE"
 for f in $( ls tcat* | sort -n ); do
     mergecmd="$mergecmd $f $f" #double each image
 done
+
 mergecmd="$mergecmd 2.0"
+echo "CMD: $mergecmd"
 eval $mergecmd
 rm -f tcat*

c8ed4f7b6b813266788585bcf36c0172e8aba012
Will Foran <will.foran@gmail.com>
2013-03-06 09:35:10 -0500

    add simOutputDir and simLogDir

--- queIncomplete/runOnWallace.bash
+++ queIncomplete/runOnWallace.bash
@@ -84,6 +84,10 @@ cat $incmpFile | while read sim unfin_type njobs jobid; do
   [[ ! $resp =~ y ]] && echo 'skipping' && continue
 
   source $sim_cfg_dir/$sim_cfg
+  SimOutputDir=$SimRoot/$sim/output
+  SimLogDir=$SimRoot/$sim/log
+  [ ! -d $SimOutputDir ] && mkdir -p $SimOutputDir
+  [ ! -d $SimLogDir ] && mkdir -p $SimLogDir
 
   ##TODO: pull this from ../qsub_possum4d.bash instead of rewritting here
   possumCmd="possum \\
@@ -98,14 +102,15 @@ cat $incmpFile | while read sim unfin_type njobs jobid; do
      --activ4D=$activ4D \\
      --activt4D=$activTime"
   
-  # check that its not already running on wallace (TODO: check that this works -- does buf_name get truncated in tmux output?)
+  # check that its not already running on wallace (TODO: check that this works -- does buf_name get truncated in tmux output? -- looks like no truncation)
   tmux list-windows | grep $buf_name 2>/dev/null && echo -e "$buf_name already running?!\n run on your own:\n $possumCmd" && continue
 
-  ## TODO: check that it hasn't already finished running on wallace
-  # ls $SCRATCH/$sim_cfg*/*$jobid 2>/dev/null && echo "already have a log file on wallace" && continue
-  
+  ## check output file doesn't already exist on wallace
+  [ -r $SimOutputDir/possum_${jobid} ] && echo "already run on wallace, remove $SimOutputDir/possum_${jobid} to rerun" && continue
+
   # run in the background and within tmux
-  echo tmux new-window -t $s_name -n $buf_name -d "$possumCmd"
+  echo "Logging to $SimLogDir/$buf_name.log"
+  echo -e "$(date +%F:%H:%M)\ntmux new-window -t $s_name -n $buf_name -d \"$possumCmd\"" | tee $SimLogDir/$buf_name.log
   echo
-  tmux new-window -t $s_name -n $buf_name -d "$possumCmd"
+  tmux new-window -t $s_name -n $buf_name -d "$possumCmd | tee -a $SimLogDir/$buf_name.log"
 done
